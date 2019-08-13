b5e7c2b048a5784df9fa57560edb726d452e2350
John Siwicki <johnsiwicki@gmail.com>
2019-08-13 10:30:22 -0400

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('css/modules/*.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+  .pipe(gulp.dest('dist'));
+});
