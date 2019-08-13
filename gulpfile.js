dfc7d400c776f0de786ce9175116c3a4b92e195e
John Siwicki <johnsiwicki@gmail.com>
2019-08-13 10:49:53 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
  
-gulp.task('minify-css', () => {
+ function minifycss() {
   return gulp.src('css/modules/*.css')
     .pipe(cleanCSS({debug: true}, (details) => {
       console.log(`${details.name}: ${details.stats.originalSize}`);
       console.log(`${details.name}: ${details.stats.minifiedSize}`);
     }))
   .pipe(gulp.dest('dist'));
-});
+ }
 
 
-const build = gulp.series(minify-css);
+const build = gulp.series(minifycss);
 exports.default = build;
